55d8e3e96d01a6330272b691e3b8b55c4800fa5f
kmkryah <kevinkryah2012@u.northwestern.edu>
2014-06-02 00:43:44 -0500

    working on js
    we seem to be having syncing problems...

---
+++ script.js
@@ -0,0 +1,87 @@
+$(".home").click(backToTop); 
+console.log("hi");
+
+function backToTop(){
+window.scrollTo(5,635);
+} 
+
+$("#boxA").click(scrollGoodRelationships);
+console.log("Yo");
+
+function scrollGoodRelationships(){
+	window.scrollTo(5,1370);
+}
+
+$("#boxB").click(scrollBadRelationships);
+console.log("Aye");
+
+function scrollBadRelationships(){
+	window.scrollTo(5,2254);
+}
+
+$("#boxC").click(scrollWorstCase);
+console.log("Hey");
+
+//function scrollWorstCase(){
+//	window.scrollTo(5,3166);
+//}
+
+function scrollWorstCase(){
+  $("body").scrollTo("#test");
+}
+
+
+$("#boxD").click(scrollLandlord);
+console.log("Hello");
+
+function scrollLandlord(){
+	window.scrollTo(5,4461);
+}
+
+//$(function() {
+//        $("#houseBurn1").hover(
+//            function() {
+//                $(this).attr("src", "http://oi61.tinypic.com/20k3far.jpg");
+//            },
+//            function() {
+//                $(this).attr("src", "http://housing.darton.edu/images/Off_Campus.png");
+//            }                         
+//        );                  
+//    });
+
+
+//$(function() {
+//        $("#houseBurn2").hover(
+//            function() {
+//                $(this).attr("src", "http://oi61.tinypic.com/20k3far.jpg");
+//            },
+//            function() {
+//               $(this).attr("src", "http://housing.darton.edu/images/Off_Campus.png");
+//            }                         
+//        );                  
+//    });
+
+
+//$(function() {
+  //      $("#houseBurn3").hover(
+    //        function() {
+      //          $(this).attr("src", "http://oi61.tinypic.com/20k3far.jpg");
+        //    },
+          //  function() {
+            //    $(this).attr("src", "http://housing.darton.edu/images/Off_Campus.png");
+          //  }                         
+      //  );                  
+  //  });
+
+
+//$(function() {
+//        $("#houseBurn4").hover(
+//            function() {
+//               $(this).attr("src", "http://oi61.tinypic.com/20k3far.jpg");
+//            },
+//            function() {
+//                $(this).attr("src", "http://housing.darton.edu/images/Off_Campus.png");
+//            }                         
+//        );                  
+//    });
+//
